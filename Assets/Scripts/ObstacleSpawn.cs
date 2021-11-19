bc28114c3bb64b796d691208c2b3879c3d0045c4
TobeTek <katchyemma@gmail.com>
2021-11-19 14:53:26 +0100

    Created Obstacle Spawning

--- Assets/Scripts/ObstacleSpawn.cs
+++ Assets/Scripts/ObstacleSpawn.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class ObstacleSpawn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float spawnRate = 1f;
+    private bool isSpawning = true;
+    public bool IsSpawning {get {return isSpawning; }}
+    private const float xLimit = 10f;
+    public GameObject firstObs;
+
+    private void Start()
     {
+        StartCoroutine(SpawnObstacles());        
+    }
+    
+    IEnumerator SpawnObstacles()
+    {
+        while(isSpawning)
+        {
+            SpawnNewObstacle();
+            yield return new WaitForSeconds(spawnRate);
+        }
         
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnNewObstacle()
     {
-        
+        Vector3 spawnLocation = new Vector3(Random.Range(-xLimit, xLimit), 0, transform.position.z);
+        Instantiate(firstObs, spawnLocation, Quaternion.identity);
     }
 }
