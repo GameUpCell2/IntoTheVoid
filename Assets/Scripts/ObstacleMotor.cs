a856d58819bb46b422ca67721b911e5a330efe50
TobeTek <katchyemma@gmail.com>
2021-11-19 14:53:26 +0100

    Created Obstacle Spawning

--- Assets/Scripts/ObstacleMotor.cs
+++ Assets/Scripts/ObstacleMotor.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class ObstacleMotor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float obstacleSpeed = 2f;
+    private float degrees = 35f;
     void Update()
     {
         
+        Vector3 to = new Vector3(degrees, 0, 0);
+        transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to, Time.deltaTime);
+        transform.position += Vector3.back * obstacleSpeed * Time.deltaTime;
+
     }
 }
